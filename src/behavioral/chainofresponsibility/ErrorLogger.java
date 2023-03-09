a7277bd9e4a345dbe0e13ca53b6dc9291f3aa5ea
Ilya Kanishev <105855090+Ilya-celebrimbor@users.noreply.github.com>
2023-03-09 16:19:25 +0600

    Chain of responsibility

---
+++ src/behavioral/chainofresponsibility/ErrorLogger.java
@@ -0,0 +1,12 @@
+package behavioral.chainofresponsibility;
+
+public class ErrorLogger extends AbstractLogger{
+    public ErrorLogger(int level) {
+        this.level = level;
+    }
+
+    @Override
+    protected void write(String message) {
+        System.out.println("Error Console Logger->Error: " + message);
+    }
+}
